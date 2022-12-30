799ac1a6bec02abc480e419c3631717613ce38f0
Mohamed Sugal <sugaal@gmail.com>
2022-12-29 19:30:42 -0600

    Implemented university suggestions based on user input

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,28 +3,65 @@ import { Container, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import "./styles.css";
 import universitiesMap from "./universities";
+import { useEffect } from "react";
 
 function App() {
-  const [universityName, setUniversityName] = useState("");
+  const [universities, setUniversities] = useState([]);
+  const [instituion, setInstituion] = useState('')
+  const [suggestions, setSuggestions] = useState([])
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const universityId = universitiesMap.get(universityName);
-    const APIKey = "weIg6spfiKIk0fC3wg5omar71jNfWG44FbgK5ghN";
-    axios
-      .get(
-        `https://api.data.gov/ed/collegescorecard/v1/schools.json?id=${universityId}&fields=school.name,2020.student.size&api_key=${APIKey}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => console.log(JSON.stringify(response.data)))
-      .catch((err) => console.log(err));
 
-    setUniversityName("");
+    // const universityId = universitiesMap.get(universityName);
+    // const APIKey = "weIg6spfiKIk0fC3wg5omar71jNfWG44FbgK5ghN";
+    // axios
+    //   .get(
+    //     `https://api.data.gov/ed/collegescorecard/v1/schools.json?id=${universityId}&fields=school.name,2020.student.size&api_key=${APIKey}`,
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   )
+    //   .then((response) => console.log(JSON.stringify(response.data)))
+    //   .catch((err) => console.log(err));
+
+    setInstituion('')
   };
+
+  useEffect(() => {
+    setUniversities(loadAllUniversities())
+    loadAllUniversities()
+  }, []) 
+
+  const loadAllUniversities = () => {
+    const universitiesMap = new Map([
+      ["University of Houston", "225511"],
+      ["Oregon State University", "209542"],
+      ["University of Massachusetts-Boston", "166638"],
+      ["Harvard University", "166027"],
+      ["University of Florida", "484473"],
+      ["Florida State University", "484173"]
+    ]);
+    let universitiesList =[ ...universitiesMap.keys()];
+    return universitiesList
+  }
+
+  const handleInstituionName = (userInput) => {
+    let matches = [];
+    console.log(userInput);
+    if (userInput.length > 0) {
+      matches = universities.filter(uni => {
+        const regex = new RegExp(`${userInput}`, "gi");
+        return uni.match(regex);
+      })
+    }
+    console.log('matches: ', matches);
+    setSuggestions(matches);
+    setInstituion(userInput);
+    console.log(userInput);
+  }
 
   // render the form and display the school data if available
   return (
@@ -37,8 +74,8 @@ function App() {
           <Form.Label>Type University Name</Form.Label>
           <Form.Control
             type="text"
-            value={universityName}
-            onChange={(e) => setUniversityName(e.target.value)}
+            value={instituion}
+            onChange={e => handleInstituionName(e.target.value)}
           />
         </Form.Group>
         <Button variant="primary" type="submit">
